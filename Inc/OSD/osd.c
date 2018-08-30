c6a0d3e8eed37b7e5b8d948250bd16bb216562ba
FeelTheLemon <feelthelemon@gmail.com>
2018-08-30 13:22:26 +0300

    little cleanup

--- Inc/OSD/osd.c
+++ Inc/OSD/osd.c
@@ -8,8 +8,6 @@
 #include "osd.h"
 #include <usart.h>
 
-#include <Sensors/sensors.h>
-
 struct sOSD_Data OSD_Data;
 
 uint32_t serialize(struct sOSD_Data* in, uint8_t* out)
@@ -97,37 +95,13 @@ void OSD_Send(void)
 	//uint8_t partA = (uint8_t)((value & 0xFF00) >> 8);
 	//uint8_t partB = (uint8_t)(value & 0x00FF);
 
-	/*data[0] = 0x0;
-	data[1] = 0x1;
-	data[2] = 0x2;
-	data[3] = 0x3;
-	data[4] = 0x4;
-	data[5] = 0x5;
-	data[6] = 0x6;
-	data[7] = 0x7;
-	data[8] = 0x8;
-	data[9] = 0x9;
-	data[10] = 0xa;
-	data[11] = 0xb;
-	data[12] = 0xc;
-	data[13] = 0xd;
-	data[14] = 0xe;
-	data[15] = 0xf;*/
-
-
-	//printf("%0.2fV \r\n", OSD_Data.V);
 
 	uint32_t data_size = serialize(&OSD_Data, data);
-	uint32_t data_size2 = deserialize(data, &OSD_Data);
+	//uint32_t data_size2 = deserialize(data, &OSD_Data);
 
 
 	//printf(" %u, %lu %lu %0.2fV %lu\r\n", sizeof(OSD_Data), data_size, data_size2, OSD_Data.V, HAL_GetTick());
 
-	//printf("data sent: %lu\r\n", data_size);
-
-
-	//HAL_UART_Transmit(&huart1, (uint8_t *)&Data, 4, 0xFFFF);
-	//printf("\r\n");
 
 	HAL_UART_Transmit(&huart2, SOD, 2, 50);
 
@@ -135,50 +109,3 @@ void OSD_Send(void)
 
 	HAL_UART_Transmit(&huart2, EOD, 2, 50);
 }
-
-
-
-/*
-
-typedef struct MSG
-{
-    int type;
-    int priority;
-    int sender;
-    char message[BUFSIZE];
-}MSG;
-
-void serialize(MSG* msgPacket, char *data)
-{
-    int *q = (int*)data;
-    *q = msgPacket->type;       q++;
-    *q = msgPacket->priority;   q++;
-    *q = msgPacket->sender;     q++;
-
-    char *p = (char*)q;
-    int i = 0;
-    while (i < BUFSIZE)
-    {
-        *p = msgPacket->message[i];
-        p++;
-        i++;
-    }
-}
-
-void deserialize(char *data, MSG* msgPacket)
-{
-    int *q = (int*)data;
-    msgPacket->type = *q;       q++;
-    msgPacket->priority = *q;   q++;
-    msgPacket->sender = *q;     q++;
-
-    char *p = (char*)q;
-    int i = 0;
-    while (i < BUFSIZE)
-    {
-        msgPacket->message[i] = *p;
-        p++;
-        i++;
-    }
-}
-*/
